ded0b33e6a6763e0d3306e3a8b0711296e741638
Jakov <jakov.gracin1@gmail.com>
2024-11-30 18:04:55 +0100

    Add square coloring

--- src/components/Grid.constants.tsx
+++ src/components/Grid.constants.tsx
@@ -1,3 +1,10 @@
 export const defaultDimension = 5;
 export const minDimension = 4;
 export const maxDimension = 10;
+
+export enum SquareCell {
+  PointA,
+  PointB,
+  Empty,
+  Obstacle,
+}
--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
 import "./Grid.scss";
-import { Button, Slider } from "@mantine/core";
-import { defaultDimension, maxDimension, minDimension } from "./Grid.constants";
+import { Slider } from "@mantine/core";
+import {
+  defaultDimension,
+  maxDimension,
+  minDimension,
+  SquareCell,
+} from "./Grid.constants";
 
-function MakeSquareMatrix(n: number): number[][] {
-  return Array.from({ length: n }).map(() => Array(n).fill(0));
+function MakeSquareMatrix(n: number): SquareCell[][] {
+  return Array.from({ length: n }).map(() => Array(n).fill(SquareCell.Empty));
 }
 
 function GetArrayOfInts(begin: number, end: number): Array<number> {
@@ -15,9 +20,39 @@ function GetArrayOfInts(begin: number, end: number): Array<number> {
   return arr;
 }
 
+// Util functions for Grid
+// function isCellPointA(grid: SquareCell[][], i: number, j: number): boolean {
+//   return grid[i][j] === SquareCell.PointA;
+// }
+// function isCellPointB(grid: SquareCell[][], i: number, j: number): boolean {
+//   return grid[i][j] === SquareCell.PointB;
+// }
+// function isCellObstacle(grid: SquareCell[][], i: number, j: number): boolean {
+//   return grid[i][j] === SquareCell.Obstacle;
+// }
+
+function getStylingOfSquare(square: SquareCell): string {
+  if (square === SquareCell.PointA) return "pointA";
+  if (square === SquareCell.PointB) return "pointB";
+  if (square === SquareCell.Obstacle) return "obstacle";
+  return "";
+}
+
+function getInlineHTMLOfSquare(square: SquareCell): string {
+  if (square === SquareCell.PointA) return "A";
+  if (square === SquareCell.PointB) return "B";
+  return "";
+}
+////////////////////////////////////////////////////////////////////////
+
 function Grid() {
   const [dimension, changeDimension] = useState(defaultDimension);
-  const grid: boolean[][] = MakeSquareMatrix(dimension);
+  const grid: SquareCell[][] = MakeSquareMatrix(dimension);
+
+  grid[0][0] = SquareCell.PointA;
+  grid[3][3] = SquareCell.PointB;
+  grid[1][1] = SquareCell.Obstacle;
+  grid[1][2] = SquareCell.Obstacle;
 
   //Slider
   const sliderMarks = [
@@ -43,9 +78,9 @@ function Grid() {
       {grid.map((row, i) => (
         <span key={i} className="row">
           {row.map((square, j) => (
-            <p key={j} className="square">
-              {/* {square} */}
-            </p>
+            <div key={j} className={"square " + getStylingOfSquare(square)}>
+              {getInlineHTMLOfSquare(square)}
+            </div>
           ))}
         </span>
       ))}

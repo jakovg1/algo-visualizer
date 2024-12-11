bb7d64cf51c9ab221e1932a66421022a40bf6248
Jakov <jakov.gracin1@gmail.com>
2024-12-11 22:57:21 +0100

    Linter hotfixes in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./variables.scss";
@@ -7,7 +6,7 @@ import App from "./App.tsx";
 import { MantineProvider } from "@mantine/core";
 
 createRoot(document.getElementById("root")!).render(
-  <MantineProvider withGlobalStyles withNormalizeCSS>
+  <MantineProvider>
     {/* <StrictMode></StrictMode> */}
     <App />
   </MantineProvider>
